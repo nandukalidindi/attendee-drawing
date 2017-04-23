3d472446c73c2a27b5c82b2aa9aba0c7afbb3324
nandukalidindi <nandukalidindi@gmail.com>
2017-04-23 05:22:16 -0400

    Update revolving strategy to slice and push one element at a time

--- src/App.js
+++ src/App.js
@@ -9,43 +9,55 @@ class App extends Component {
     users: []
   }
 
-  componentDidMount() {
-    var userData =  [["Desmond", "Strickland", "Fishery"],
-                     ['Thaddeus','Galvan','Professional Training & Coaching'],
-                     ['Lamont','Friedman','Automotive'],
-                     ['Martin','Holland','Information Technology and Services'],
-                     ['Nancy','Carr','Information Technology and Services'],
-                     ['Graham','Norris',' Ceramics & Concrete'],
-                     ['Royce','Lester','Veterinary'],
-                     ['Emily','Herman','Alternative Dispute Resolution'],
-                     ['Odessa','Clay','Warehousing'],
-                     ['Enid','Castaneda','Airlines/Aviation'],
-                     ['Mara','Schultz','Facilities Services'],
-                     ['Romeo','Navarro','Information Technology and Services'],
-                     ['Jess','Clay','Banking'],
-                     ['Aimee','Guerrero','Biotechnology'],
-                     ['Dino','Payne','Computer Games'],
-                     ['Susie','Velasquez','Nanotechnology'],
-                     ['Velma','Walton','Restaurants'],
-                     ['Rosario','Cook','Professional Training & Coaching'],
-                     ['Maximo','Morgan','Transportation/Trucking/Railroad'],
-                     ['Bettye','Sandoval','Tobacco'],
-                     ['Eve','Malone','Judiciary'],
-                     ['Daryl','Carney','Real Estate'],
-                     ['Britney','Pennington','Recreational Facilities and Services'],
-                     ['Phyllis','Chung','International Affairs']]
+  userData =  [["Desmond", "Strickland", "Fishery"],
+                   ['Thaddeus','Galvan','Professional Training & Coaching'],
+                   ['Lamont','Friedman','Automotive'],
+                   ['Martin','Holland','Information Technology and Services'],
+                   ['Nancy','Carr','Information Technology and Services'],
+                   ['Graham','Norris',' Ceramics & Concrete'],
+                   ['Royce','Lester','Veterinary'],
+                   ['Emily','Herman','Alternative Dispute Resolution'],
+                   ['Odessa','Clay','Warehousing'],
+                   ['Enid','Castaneda','Airlines/Aviation'],
+                   ['Mara','Schultz','Facilities Services'],
+                   ['Romeo','Navarro','Information Technology and Services'],
+                   ['Jess','Clay','Banking'],
+                   ['Aimee','Guerrero','Biotechnology'],
+                   ['Dino','Payne','Computer Games'],
+                   ['Susie','Velasquez','Nanotechnology'],
+                   ['Velma','Walton','Restaurants'],
+                   ['Rosario','Cook','Professional Training & Coaching'],
+                   ['Maximo','Morgan','Transportation/Trucking/Railroad'],
+                   ['Bettye','Sandoval','Tobacco'],
+                   ['Eve','Malone','Judiciary'],
+                   ['Daryl','Carney','Real Estate'],
+                   ['Britney','Pennington','Recreational Facilities and Services'],
+                   ['Phyllis','Chung','International Affairs']]
 
-    userData.forEach(function(user, index) {
+  componentWillMount() {
+    this.userData.slice(this.state.counter % this.userData.length, this.state.counter % this.userData.length + 6).forEach(function(user, index) {
       this.state.users.push(
-        <figure key={index}>{this.userHtml(user[0], user[1], user[2])}</figure>
-      )
+        <figure key={index}> {this.userHtml(user[0], user[1], user[2])}</figure>
+      );
     }.bind(this));
+  }
+
+  componentDidMount() {
     setInterval(() => this.updateCounter(), 1000);
   }
 
   updateCounter() {
+    var currentOffset = this.state.counter % this.userData.length;
+
+    var newUsers = this.state.users.slice(1);
+
+    newUsers.push(
+      <figure key={currentOffset}> {this.userHtml(this.userData[currentOffset][0], this.userData[currentOffset][1], this.userData[currentOffset][2])} </figure>
+    );
+
     this.setState({
-      counter: (new Date()).getSeconds()
+      counter: (new Date()).getSeconds(),
+      users: newUsers
     })
   }
 
@@ -65,13 +77,12 @@ class App extends Component {
   render() {
     return (
       <div id="attendee-drawing-overlay" className="overlay" style={{width: "100%"}}>
-        <h2>It is {this.state.counter}.</h2>
         <a href="javascript:void(0)" className="closebtn" style={{zIndex: "1000"}} >&times;</a>
 
         <div className="overlay-content container" style={{marginTop: "15%"}}>
             <div className="col-md-12 col-lg-8 col-lg-offset-2" id="carousel-wrapper">
                 <div id="carousel" style={{"WebkitTransform": "rotateX(" + this.state.counter * -60 + "deg)"}}>
-                    {this.state.users}
+                  {this.state.users}
                 </div>
             </div>
 
