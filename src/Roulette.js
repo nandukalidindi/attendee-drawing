0e2cc891f3d929d9f50667e945b5e436836f6fc6
nandukalidindi <nandukalidindi@gmail.com>
2017-04-24 03:10:41 -0400

    Remove all the un-necessary code, well is documentation also un-necessary?

--- src/Roulette.js
+++ src/Roulette.js
@@ -53,14 +53,14 @@ class Roulette extends Component {
       arc.attr("fill", this.getColor(i, this.multiplyList(pieText).length));
       arcs.push(arc);
     }
-    //draw text on top of arcs
+    
     for(var i=0; i <this.multiplyList(pieText).length; i++){
       startAngle = endAngle;
       endAngle = startAngle + 360/this.multiplyList(pieText).length;
       var text = paper.text(center.x + diameter/2, center.y, this.multiplyList(pieText)[i]);
       text.attr({"font-size": "20px"});
       text.transform('r'+(startAngle+endAngle)/2 + ' ' + center.x + ' ' + center.y);
-      //alert(d);
+
       texts.push(text);
     }
 
@@ -70,26 +70,26 @@ class Roulette extends Component {
     p.glow({width:5, offsetx:2.5, offsety:2.5});
 
 
-    var time = 8000; //ms
-    //var easing = '>'
+    var time = 8000;
+
     var easing = 'cubic-bezier(0,1,0.1,1)' ;
     var rotateAngle = 360 * 9;
-    //var rotateAngle = 360 * 1;
+
     rotateAngle -= this.getAngleFromID(2, this.multiplyList(pieText).length);
     rotateAngle += this.getRandomDriftDeg(this.multiplyList(pieText));
-    // spinToId texts
+
     texts.forEach(function(text){
       var fromAngle = parseInt(text.transform()[0][1]);
       var toAngle = fromAngle + rotateAngle;
       text.stop().animate({transform: "r" + toAngle + " " + center.x + " " + center.y}, time, easing);
     });
-    // spinToId arcs
+
     var roulette = paper.set(arcs);
     roulette.stop().animate({transform: "r" + rotateAngle + " " + center.x + " " + center.y}, time, easing);
 
   }
 
-  getAngleFromID(arcId, arcsCount){ // Do we need arcsCount?
+  getAngleFromID(arcId, arcsCount){
     var arcAngle = 360/arcsCount;
     return (arcAngle * arcId + arcAngle/2);
   }
@@ -105,7 +105,7 @@ class Roulette extends Component {
     while (list.indexOf("") > 0){
       list.splice(list.indexOf(""),1);
     }
-    // Repeat items until it has more than 8 items
+
     while (list.length < 8){
       list = list.concat(list);
     }
@@ -115,8 +115,6 @@ class Roulette extends Component {
   getColor(i, total){
     var h = i/total;
     return "hsl(" + h + ", .7, 0.5)";
-    //return colorArr[i % total % colorArr.length];
-    //return colorArr[i];
   }
 
   rotateRoulette(event) {
@@ -149,34 +147,6 @@ class Roulette extends Component {
       </div>
     );
   }
-
-  // render(){
-  //       var data = [
-  //           {x:50,y:50,r:40,attr:{"stroke":"#0b8ac9","stroke-width":5},animate:Raphael.animation({cx:60},500,"<>")},
-  //           {x:100,y:100,r:40,attr:{"stroke":"#f0c620","stroke-width":5},animate:Raphael.animation({cx:105},500,"<>")},
-  //           {x:150,y:50,r:40,attr:{"stroke":"#1a1a1a","stroke-width":5}},
-  //           {x:200,y:100,r:40,attr:{"stroke":"#10a54a","stroke-width":5},animate:Raphael.animation({cx:195},500,"<>")},
-  //           {x:250,y:50,r:40,attr:{"stroke":"#e11032","stroke-width":5},animate:Raphael.animation({cx:240},500,"<>")}
-  //       ]
-  //       return (<Paper width={300} height={300}>
-  //                      <Set>
-  //                       {
-  //                           data.map(function(ele,pos){
-  //                               return (<Circle key={pos} x={ele.x} y={ele.y} r={ele.r} attr={ele.attr} animate={ele.animate}/>)
-  //                           })
-  //                       }
-  //                       </Set>
-	// 					<Set>
-  //                           <Rect x={30} y={148} width={240} height={150} attr={{"fill":"#10a54a","stroke":"#f0c620","stroke-width":5}}/>
-	// 						<Ellipse x={150} y={198} ry={40} rx={100} attr={{"fill":"#fff","stroke":"#e11032"}} glow={{width:10,fill:true,color:"#0b8ac9",opacity:1}}/>
-  //                           <Image src="static/images/5circle.png" x={100} y={170} width={90} height={60} />
-	// 						<Text x={150} y={258} text="同一个世界 同一个梦想" attr={{"fill":"#fff"}}/>
-	// 						<Text x={150} y={273} text="One World One Dream" attr={{"fill":"#fff"}}/>
-	// 						<Path d={["M150 287L150 287"]} animate={Raphael.animation({"path": ["M80 287L220 287"]},500,"<>")} attr={{"stroke":"#fff"}}/>
-  //                           <Line x1={150} y1={290} x2={150} y2={290} animate={Raphael.animation({ x1:80, x2:220},500,"<>")} attr={{"stroke":"#fff"}}/>
-	// 					</Set>
-  //               </Paper>)
-  //   }
 }
 
 export default Roulette;
